22598813b7c2a19bd9e9a6c191ee9f070e0996d9
Sam Welborn <swelborn@lbl.gov>
2025-10-21 18:50:51 -0700

    pm updates

--- iperf/scripts/perlmutter/cpu-cpu/piperf.sh
+++ iperf/scripts/perlmutter/cpu-cpu/piperf.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=2
 #SBATCH --ntasks-per-node=1
 #SBATCH --qos=debug
-#SBATCH --time=00:30:00
+#SBATCH --time=00:15:00
 #SBATCH --constraint=cpu
 #SBATCH --account=nstaff
 #SBATCH --output=%j.out
@@ -16,13 +16,14 @@ server_node=${nodes_array[0]}
 server_node_hostname=${server_node}.chn.perlmutter.nersc.gov
 client_node=${nodes_array[1]}
 
-CPU_NODE_NIC_NUMA=2
+export PIPERF3_RUN_ID=$SLURM_JOB_ID
 
-srun --nodes=1 --ntasks=1 -w ${server_node} numactl --cpunodebind=${CPU_NODE_NIC_NUMA} \
-    uv run piperf3 server &
+srun --nodes=1 --ntasks=1 -w ${server_node} uv run piperf3 server &
 SERVER_PID=$!
 
-srun --nodes=1 --ntasks=1 -w ${client_node} numactl --cpunodebind=${CPU_NODE_NIC_NUMA} \
-    uv run piperf3 client ${server_node_hostname}
+# a little sleep to ensure server starts before client connection
+sleep 5
+
+srun --nodes=1 --ntasks=1 -w ${client_node} uv run piperf3 client ${server_node_hostname}
 
 kill $SERVER_PID 2>/dev/null
